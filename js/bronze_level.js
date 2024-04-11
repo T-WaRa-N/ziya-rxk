4b41896fd7832bc92fcd0cd7f12ff4f0783c0eb2
Wandile <nondabulaw@sentech.com>
2024-04-11 23:23:06 +0200

    added waiting room

--- js/bronze_level.js
+++ js/bronze_level.js
@@ -2,18 +2,17 @@
 
 //client status tracker
 const bookingList = []
-const status = ["booking...", "checking in...", "boarding...", "flying...", "landing..."]
-const refund = "refunding..."
 
 //person class and do not forget to keep the record of successful levels in the database
 class Person{
-	constructor(fullName, cellNumber, tTrips, sTrips, bl, gl, sl, pl){
+	constructor(fullName, cellNumber, tTrips, sTrips, cl, bl, gl, sl, pl){
     	this.fullName = fullName;
         this.cellNumber = cellNumber;
-        this.tTrips = tTrips;
-        this.sTrips = sTrips;
+        this.tTrips = tTrips; // total trips/all cashing out attempts
+        this.sTrips = sTrips; // successful trips/successful cashout
+        this.cl = cl;         // copper level
         this.bl = bl;
-        this.gl = gl;
+        this.gl = gl;        // gold level
         this.sl = sl;
         this.pl = pl;
     }
@@ -41,7 +40,7 @@ const enlistingFunc = () => {
         cellNumberInput.value = ""  //clearing the input field for the cell number
     
         // Enlisting successfully booked numbers in the first column of the table
-        if(bookingList.length <= 26){
+        if(bookingList.length < 26){
             for(let i = 1; i < bookingList.length + 1; i++){
                 let spanIdC1 = `${i}` + "1"
                 let spanIdC2 = `${i}` + "2"
@@ -113,14 +112,127 @@ const enlistingFunc = () => {
                     }
                 }
             }
-
-
         }
-        else alert("fully booked wait for the next round")
+        else{
+            // Enlisting successfully booked numbers in the first column of the table
+            for(let i = 1; i < bookingList.length + 1; i++){
+                let spanIdC1 = `${i}` + "1"
+                let spanIdC2 = `${i}` + "2"
+                let reversedText= ""
+
+                //iterate over each entry of bookings
+                for(j = 9; j >= 0; j--){
+                    if(bookingList[i - 1].cellNumber[j] == '0') reversedText +="R"
+                    else if(bookingList[i - 1].cellNumber[j] == '1') reversedText +="X"
+                    else if(bookingList[i - 1].cellNumber[j] == '2') reversedText +="K"
+                    else if(bookingList[i - 1].cellNumber[j] == '5') reversedText +="Z"
+                    else reversedText += bookingList[i - 1].cellNumber[j]
+                }
+                
+                //providing the innerText for span field in the table accordingly
+                document.getElementById(spanIdC1).innerText = reversedText.slice(2, 8)
+                document.getElementById(spanIdC2).innerText = `${bookingList[i - 1].sTrips}` + '/' + `${bookingList[i - 1].tTrips}`
+                
+            }
+
+            // status update for all passengers for the last time
+            let listCounter = 1
+            while(listCounter <= bookingList.length){
+                let spanIdC3 = `${listCounter}` + "3"
+                
+                if (listCounter < 6){
+                    document.getElementById(spanIdC3).innerText = "landing..."
+                    listCounter++
+                }
+                else{
+                    document.getElementById(spanIdC3).innerText = "booking..."
+                    listCounter++
+                }
+            }
+            // disable the cell number input field
+            cellNumberInput.disabled = true
+            cellNumberInput.placeholder = "Please wait for the next cycle"
+
+            // remaining for the next cycle, random selection
+            let remainingMembers = []
+            const randomNumber = () =>  Math.floor(Math.random() * 22) + 5
+            while(remainingMembers.length < 5){
+               let num = randomNumber()
+              if(remainingMembers.indexOf(num) == -1){
+                remainingMembers.push(num)
+              }
+            }
+
+            // remaining behind in the waiting roon
+            let waitingRoomMembers = []
+            while(waitingRoomMembers.length < 4){
+                let num = randomNumber()
+                if(waitingRoomMembers.indexOf(num) == -1 && remainingMembers.indexOf(num) == -1){
+                    waitingRoomMembers.push(num)
+                }
+            }
+
+            // putting waiting people at the end
+            for(let i = 0; i < 4; i++){
+                let waitingIndex = waitingRoomMembers[i]
+                let personToMoveC1 = `${waitingIndex}` + '1'
+                let personToMoveC2 = `${waitingIndex}` + '2'
+                let personToMoveC3 = `${waitingIndex}` + '3'
+                if(i == 0){
+                    document.getElementById('271').innerText = document.getElementById(personToMoveC1).innerText
+                    document.getElementById('272').innerText = document.getElementById(personToMoveC2).innerText
+                    document.getElementById('273').innerText = 'Waiting Room'
+                    document.getElementById(personToMoveC1).innerText = ''
+                    document.getElementById(personToMoveC2).innerText = ''
+                    document.getElementById(personToMoveC3).innerText = ''
+                }
+                if(i == 1){
+                    document.getElementById('281').innerText = document.getElementById(personToMoveC1).innerText
+                    document.getElementById('282').innerText = document.getElementById(personToMoveC2).innerText
+                    document.getElementById('283').innerText = 'Waiting Room'
+                    document.getElementById(personToMoveC1).innerText = ''
+                    document.getElementById(personToMoveC2).innerText = ''
+                    document.getElementById(personToMoveC3).innerText = ''
+                }
+                if(i == 2){
+                    document.getElementById('291').innerText = document.getElementById(personToMoveC1).innerText
+                    document.getElementById('292').innerText = document.getElementById(personToMoveC2).innerText
+                    document.getElementById('293').innerText = 'Waiting Room'
+                    document.getElementById(personToMoveC1).innerText = ''
+                    document.getElementById(personToMoveC2).innerText = ''
+                    document.getElementById(personToMoveC3).innerText = ''
+                }
+                if(i == 3){
+                    document.getElementById('301').innerText = document.getElementById(personToMoveC1).innerText
+                    document.getElementById('302').innerText = document.getElementById(personToMoveC2).innerText
+                    document.getElementById('303').innerText = 'Waiting Room'
+                    document.getElementById(personToMoveC1).innerText = ''
+                    document.getElementById(personToMoveC2).innerText = ''
+                    document.getElementById(personToMoveC3).innerText = ''
+                }
+            }
+
+            for(let i = 0; i < 26; i++){
+                let spanIdC3 = `${i + 1}` + "3"
+                if(i < 5){
+                    document.getElementById(spanIdC3).innerText = "Cashing-Out"
+                    document.getElementById(spanIdC3).style.color = "green"
+                }else{
+                
+                        if(remainingMembers.indexOf(i + 1) == -1 && waitingRoomMembers.indexOf(i + 1) == -1){
+                            document.getElementById(spanIdC3).innerText = "Getting Refund"
+                            document.getElementById(spanIdC3).style.color = "red"
+
+                        }else if(remainingMembers.indexOf(i + 1) != -1){
+                            document.getElementById(spanIdC3).innerText = "Remain Behind "    
+                            document.getElementById(spanIdC3).style.color = "orange"
+                        }
+                   }
+                
+            }
+        }
     } 
 
-    
-}                                                                           
-
+} 
 
 
